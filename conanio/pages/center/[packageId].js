374c4b77ab67d350d636e7c9e616a7d05cfe0be3
Francisco Ramirez de Anton <franchuti688@gmail.com>
2023-06-14 11:12:34 +0200

    Working if/else markdown!

--- conanio/pages/center/[packageId].js
+++ conanio/pages/center/[packageId].js
@@ -34,6 +34,13 @@ export async function getServerSideProps(context) {
   }
 }
 
+function RenderedMarkdown({md}) {
+  if (typeof md === 'undefined') {
+    return "It was not possible to load this information. Please, check if this recipe version is compatible with Conan v2.x.";
+  }
+  return <ReactMarkdown>{md}</ReactMarkdown>;
+}
+
 
 export default function ConanPackage(props) {
   const [selectedVersion, setSelectedVersion] = useState(Object.keys(props.data)[0]);
@@ -51,51 +58,52 @@ export default function ConanPackage(props) {
           <h1 className="text-center" >Conan {props.packageId} package site</h1>
           <br/>
           <Row>
-          <Col xs lg="8">
-          <Row>
-            <Col>
-              <h3>
-                {props.data[selectedVersion].name}
-              </h3>
+            <Col xs lg="8">
+              <Row>
+                <Col>
+                  <h3>
+                    {props.data[selectedVersion].name}
+                  </h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs lg="4">
+                  <Form.Select size="sm" value={selectedVersion} onChange={handleChange}>
+                      {Object.keys(props.data).map((version) => (<option key={version} value={version}>Version: {version}</option>))}
+                    </Form.Select>
+                </Col>
+                <Col xs lg="5"><p><b>Licenses:</b> {props.data[selectedVersion].info.licenses.join(", ")}</p></Col>
+                { props.data[selectedVersion].info.downloads > 0 && <Col xs lg="3"><p><b>Downloads:</b> {props.data[selectedVersion].info.downloads}</p></Col> }
+              </Row>
+              <Row>
+                <Col xs lg><p><b>Description:</b> {props.data[selectedVersion].info.description}</p></Col>
+              </Row>
+              <Row>
+                <Col xs lg><p><b>Labels:</b> {props.data[selectedVersion].info.labels.map((item) => (<Badge key={item}>{item}</Badge>))}</p></Col>
+              </Row>
+              <Row>
+                <Col xs lg><Link href={"https://github.com/conan-io/conan-center-index/tree/master/recipes/" + props.data[selectedVersion].name}><a><p>{props.data[selectedVersion].name} recipe</p></a></Link></Col>
+              </Row>
             </Col>
-          </Row>
-          <Row>
+            { props.data[selectedVersion].info.downloads > 0 && 
             <Col xs lg="4">
-               <Form.Select size="sm" value={selectedVersion} onChange={handleChange}>
-                  {Object.keys(props.data).map((version) => (<option key={version} value={version}>Version: {version}</option>))}
-                </Form.Select>
-            </Col>
-            <Col xs lg="5"><p><b>Licenses:</b> {props.data[selectedVersion].info.licenses.join(", ")}</p></Col>
-            { props.data[selectedVersion].info.downloads > 0 && <Col xs lg="3"><p><b>Downloads:</b> {props.data[selectedVersion].info.downloads}</p></Col> }
+                <LineChart width={400} height={200} data={props.downloads[selectedVersion].downloads} margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
+                  <XAxis dataKey="date" />
+                  <Tooltip />
+                  <CartesianGrid stroke="#f5f5f5" />
+                  <Line type="monotone" dataKey="downloads" stroke="#0d6efd" yAxisId={0} />
+                </LineChart>
+            </Col>}
           </Row>
-          <Row>
-            <Col xs lg><p><b>Description:</b> {props.data[selectedVersion].info.description}</p></Col>
-          </Row>
-          <Row>
-            <Col xs lg><p><b>Labels:</b> {props.data[selectedVersion].info.labels.map((item) => (<Badge key={item}>{item}</Badge>))}</p></Col>
-          </Row>
-          <Row>
-            <Link href={"https://github.com/conan-io/conan-center-index/tree/master/recipes/" + props.data[selectedVersion].name}><a><p>{props.data[selectedVersion].name} recipe</p></a></Link>
-          </Row>
-          </Col>
-          { props.data[selectedVersion].info.downloads > 0 && <Col xs lg="4">
-            <LineChart width={400} height={200} data={props.downloads[selectedVersion].downloads} margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
-              <XAxis dataKey="date" />
-              <Tooltip />
-              <CartesianGrid stroke="#f5f5f5" />
-              <Line type="monotone" dataKey="downloads" stroke="#0d6efd" yAxisId={0} />
-            </LineChart>
-          </Col>}
-          </Row>
-          <Row>
-            <Tabs defaultActiveKey="use-it" id="uncontrolled">
-              <Tab eventKey="use-it" title="Use it"><br/><ReactMarkdown>{props.tabs.md[selectedVersion].md}</ReactMarkdown></Tab>
-              {false && (<Tab eventKey="packages" title="Packages"><br/><ReactMarkdown>{props.tabs.packages[selectedVersion].md}</ReactMarkdown></Tab>)}
-              {false && (<Tab eventKey="examples" title="Examples"><br/><ReactMarkdown>{props.tabs.example[selectedVersion].md}</ReactMarkdown></Tab>)}
-              {false && (<Tab eventKey="options" title="Options"><br/><ReactMarkdown>{props.tabs.options[selectedVersion].md}</ReactMarkdown></Tab>)}
-              <Tab eventKey="badges" title="Badges"><br/><ReactMarkdown>{props.tabs.shields_io[selectedVersion].md}</ReactMarkdown></Tab>
-            </Tabs>
-          </Row>
+          <Tabs defaultActiveKey="use-it" id="uncontrolled">
+            <Tab eventKey="use-it" title="Use it"><br/><RenderedMarkdown md={props.tabs.md[selectedVersion].md} /></Tab>
+            <Tab eventKey="badges" title="Badges"><br/><ReactMarkdown>{props.tabs.shields_io[selectedVersion].md}</ReactMarkdown></Tab>
+            {/*
+            <Tab eventKey="packages" title="Packages"><br/><ReactMarkdown>{props.tabs.packages[selectedVersion].md}</ReactMarkdown></Tab>
+            <Tab eventKey="examples" title="Examples"><br/><ReactMarkdown>{props.tabs.example[selectedVersion].md}</ReactMarkdown></Tab>
+            <Tab eventKey="options" title="Options"><br/><ReactMarkdown>{props.tabs.options[selectedVersion].md}</ReactMarkdown></Tab>
+            */}
+          </Tabs>
         </Container>
         <br/>
         <ConanFooter/>
