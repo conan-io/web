a04d56538c4ed80c1a9ba0272a5836c7cee69bc8
David Sanchez <davidsf@jfrog.com>
2023-06-28 18:46:42 +0200

    bg main container fixed

--- conanio/pages/center/[packageId].js
+++ conanio/pages/center/[packageId].js
@@ -57,7 +57,7 @@ export default function ConanPackage(props) {
   return (
     <React.StrictMode>
       <SSRProvider>
-      <div className="flex-wrapper">
+      <div className="flex-wrapper bg-conan-blue">
         <ConanCenterHeader/>
         <br/>
         <Container className="conancontainer">
