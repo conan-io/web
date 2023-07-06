4fc58afd4a2b3a9010bb12519731b9099de615a9
David Sanchez <davidsf@jfrog.com>
2023-07-06 20:31:37 +0200

    props.data[selectedVersion].info.use_it added

--- conanio/pages/center/packages/[packageId].js
+++ conanio/pages/center/packages/[packageId].js
@@ -9,7 +9,6 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Badge from 'react-bootstrap/Badge';
 import Link from 'next/link';
-import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import cmake from 'highlight.js/lib/languages/cmake';
 import makefile from 'highlight.js/lib/languages/makefile';
@@ -17,6 +16,7 @@ import { ConanCenterHeader } from '../../../components/header';
 import ConanFooter from '../../../components/footer';
 import {LineChart, XAxis, Tooltip, CartesianGrid, Line} from 'recharts';
 import {get_json, get_urls} from '../../../service/service';
+import { DefaultDescription } from '../packages';
 import { LiaBalanceScaleSolid, LiaGithub } from "react-icons/lia";
 
 
@@ -27,12 +27,6 @@ export async function getServerSideProps(context) {
     props: {
       data: data,
       downloads: await get_json(urls.package.downloads, urls.api.private),
-      tabs: {
-        md: await get_json(urls.package.md, urls.api.private),
-        example: await get_json(urls.package.example, urls.api.private),
-        options: await get_json(urls.package.options, urls.api.private),
-        packages: await get_json(urls.package.packages, urls.api.private),
-      },
       packageId: context.params.packageId,
     },
   }
@@ -43,7 +37,7 @@ function RenderedMarkdown({md}) {
   if (typeof md === 'undefined') {
     return "It was not possible to load this information. Please, check if this recipe version is compatible with Conan v2.x.";
   }
-  return <ReactMarkdown rehypePlugins={[[rehypeHighlight, {languages: {cmake, makefile}, detect: true, ignoreMissing: true}]]}>{md}</ReactMarkdown> ;
+  return <div><pre>{JSON.stringify(md, null, 2)}</pre></div>;
 }
 
 
@@ -89,43 +83,37 @@ export default function ConanPackage(props) {
                 <br/>
                 </Col>
               </Row>
-              <Row>
+              {props.data[selectedVersion].info.description && (<Row>
                 <Col className="mb-2" xs lg>{props.data[selectedVersion].info.description}</Col>
-              </Row>
-              <Row>
+              </Row>)}
+              {props.data[selectedVersion].info.licenses && props.data[selectedVersion].info.licenses.length > 0 && (<Row>
                 <Col xs lg="8"><LiaBalanceScaleSolid className="conanIconBlue"/> {props.data[selectedVersion].info.licenses.join(", ")}</Col>
-              </Row>
-              <Row>
-                { ((props.downloads[selectedVersion].downloads && props.downloads[selectedVersion].downloads.length > 0) ||
-                  props.data[selectedVersion].info.downloads > 0) &&
-                  <Col xs lg="8"><b>Downloads:</b> {props.data[selectedVersion].info.downloads}</Col> }
-              </Row>
-              <Row>
+              </Row>)}
+              {((props.downloads[selectedVersion].downloads && props.downloads[selectedVersion].downloads.length > 0) || props.data[selectedVersion].info.downloads > 0) && (<Row>
+                <Col xs lg="8"><b>Downloads:</b> {props.data[selectedVersion].info.downloads}</Col>
+              </Row>)}
+              {props.data[selectedVersion].info.description && (<Row>
                 <Col xs lg className="mb-2"><Link href={"https://github.com/conan-io/conan-center-index/tree/master/recipes/" + props.data[selectedVersion].name}><a><LiaGithub className="conanIconBlue"/>Recipe source</a></Link></Col>
-              </Row>
-              <Row>
+              </Row>)}
+              {props.data[selectedVersion].info.labels && props.data[selectedVersion].info.labels.length > 0 && (<Row>
                 <Col xs lg><p> {props.data[selectedVersion].info.labels.map((item) => (<Badge key={item}>#{item}</Badge>))}</p></Col>
-              </Row>
+              </Row>)}
             </Col>
             { props.downloads[selectedVersion].downloads && props.downloads[selectedVersion].downloads.length > 0 &&
-              <Col xs lg="4">
-                <LineChart width={400} height={200} data={props.downloads[selectedVersion].downloads} margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
-                  <XAxis dataKey="date" />
-                  <Tooltip />
-                  <CartesianGrid stroke="#f5f5f5" />
-                  <Line type="monotone" dataKey="downloads" stroke="#0d6efd" yAxisId={0} />
-                </LineChart>
-              </Col> }
-            </Row>
-          <Tabs className="package-tabs" defaultActiveKey="use-it" id="uncontrolled">
-            <Tab eventKey="use-it" title="Use it"><br/><RenderedMarkdown md={props.tabs.md[selectedVersion].md} /></Tab>
+            <Col xs lg="4">
+              <LineChart width={400} height={200} data={props.downloads[selectedVersion].downloads} margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
+                <XAxis dataKey="date" />
+                <Tooltip />
+                <CartesianGrid stroke="#f5f5f5" />
+                <Line type="monotone" dataKey="downloads" stroke="#0d6efd" yAxisId={0} />
+              </LineChart>
+            </Col> }
+          </Row>
+          {!props.data[selectedVersion].info.description && (<DefaultDescription name={props.data[selectedVersion].name}/>)}
+          {props.data[selectedVersion].info.description && (<Tabs className="package-tabs" defaultActiveKey="use-it" id="uncontrolled">
+            <Tab eventKey="use-it" title="Use it"><br/><RenderedMarkdown md={props.data[selectedVersion].info.use_it} /></Tab>
             <Tab eventKey="badges" title="Badges"><br/><BadgesTab packageName={props.packageId} /></Tab>
-            {/*
-            <Tab eventKey="packages" title="Packages"><br/><ReactMarkdown>{props.tabs.packages[selectedVersion].md}</ReactMarkdown></Tab>
-            <Tab eventKey="examples" title="Examples"><br/><ReactMarkdown>{props.tabs.example[selectedVersion].md}</ReactMarkdown></Tab>
-            <Tab eventKey="options" title="Options"><br/><ReactMarkdown>{props.tabs.options[selectedVersion].md}</ReactMarkdown></Tab>
-            */}
-          </Tabs>
+          </Tabs>)}
         </Container>
         <br/>
         <ConanFooter/>
