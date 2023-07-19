7d44af2c848b30bd24c3c65ded5055efe6270d1c
Francisco Ramirez de Anton <franchuti688@gmail.com>
2023-07-19 16:50:43 +0200

    Added packageTabs as separate module. Added UseItTab which creates the 'Use it' information

--- conanio/pages/_app.js
+++ conanio/pages/_app.js
@@ -9,48 +9,10 @@ import '../styles/index.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+import "highlight.js/styles/github.css";
+
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 export default MyApp
-
-// export function MyApp({ Component, pageProps }) {
-//   const [loading, setLoading] = useState(false);
-//   React.useEffect(() => {
-//     const start = () => {
-//       console.log("start");
-//       setLoading(true);
-//     };
-//     const end = () => {
-//       console.log("findished");
-//       setLoading(false);
-//     };
-//     Router.events.on("routeChangeStart", start);
-//     Router.events.on("routeChangeComplete", end);
-//     Router.events.on("routeChangeError", end);
-//     return () => {
-//       Router.events.off("routeChangeStart", start);
-//       Router.events.off("routeChangeComplete", end);
-//       Router.events.off("routeChangeError", end);
-//     };
-//   }, []);
-//   return (
-//     <>
-//       {loading ? (
-//         <div className="flex-wrapper">
-//           <ConanKitchenHeader/>
-//           <div className="text-center">
-//             <div className="spinner-grow conan-loading text-primary">
-//             </div>
-//           </div>
-//           <ConanFooter/>
-//         </div>
-//       ) : (
-//         <Component {...pageProps} />
-//       )}
-//     </>
-//   );
-// }
-//
-// export default MyApp
