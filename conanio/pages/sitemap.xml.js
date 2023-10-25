e75ebb065803aaa18b48b083aa985b7f018029fe
Rubén Rincón <rubenrb@jfrog.com>
2023-10-25 13:40:35 +0200

    Typo

--- conanio/pages/sitemap.xml.js
+++ conanio/pages/sitemap.xml.js
@@ -7,7 +7,7 @@ const URL = "https://conan.io"
 function generateSiteMap(packages) {
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
+     <!--We manually set the URLs we know already-->
      <url>
         <loc>${URL}/center</loc>
      </url>
