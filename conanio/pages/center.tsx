34400eaa541e6cef7175fcd34c8d367f80661aba
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-10-07 17:30:21 +0200

    Added Conan 1.x banner

--- conanio/pages/center.tsx
+++ conanio/pages/center.tsx
@@ -40,6 +40,33 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 
+function Conan1xBanner() {
+  return (
+    <section id="global-banner" className="global-banner">
+      <div className="container">
+        <div className="row d-flex justify-content-around align-items-center">
+          <div className="col-auto mt-1 mb-1 text-center text-white">
+            Conan Center will stop receiving updates for Conan 1.x packages soon.
+              <Link href="https://blog.conan.io/2024/09/30/Conan-Center-will-stop-receiving-updates-for-Conan-1.html" onClick={
+                      () => {
+                        window.dataLayer.push({
+                          'event': 'fireEvent',
+                          'event_name': 'element_click',
+                          'type': 'ui',
+                          'purpose': 'feedback',
+                          'description': 'conancenter stop 1.x packages'
+                        });
+                      }
+                    } className="white"> <b>Read this blog post</b>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
 const CenterList = (props: { name: string; extraInfo: string; data: RecipeBasic[]; isFullName: boolean }) => (
     <div className="text-center">
       <Tooltip id="extra-info" />
@@ -77,6 +104,7 @@ const Center: NextPage<PageProps> = (props) => (
 
     <div className="flex-wrapper bg-conan-blue">
       <ConanCenterHeader/>
+      <Conan1xBanner/>
         <br/>
         <Container className="conancontainer">
           <Container><h1 className="text-center">The Conan libraries and tools central repository</h1></Container>
