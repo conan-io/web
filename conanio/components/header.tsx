e8b01e44b687c9935347bc56c3f5ac2083a4cf1d
Francisco Ramirez de Anton <franchuti688@gmail.com>
2025-04-29 15:10:11 +0200

    wip

--- conanio/components/header.tsx
+++ conanio/components/header.tsx
@@ -42,6 +42,7 @@ const ConanHead = (props: {titlePrefix?: string}) => {
         <link rel="canonical" href={canonicalUrl} />
       </Head>
       <Script
+          strategy="beforeInteractive"
           data-cfasync="false"
           data-tracker-overrides="GoogleConsentMode:security_storage=on;ad_storage=SaleOfInfo,Advertising;ad_user_data=SaleOfInfo,Advertising;ad_personalization=SaleOfInfo,Advertising;analytics_storage=Analytics,SaleOfInfo;functionality_storage=Functional,SaleOfInfo;personalization_storage=Functional,SaleOfInfo"
           src="https://transcend-cdn.com/cm/f0071674-c641-4cf3-9d31-303ec0c86b1b/airgap.js"
