94a602b17ca0eb9e775b89135903764ef178f9f0
danimtb <danimanzaneque@gmail.com>
2025-05-12 17:59:25 +0200

    add mocks

--- conanio/jest.setup.js
+++ conanio/jest.setup.js
@@ -4,3 +4,19 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+jest.mock('react-markdown', () => require('./tests/mocks/react-markdown'));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
